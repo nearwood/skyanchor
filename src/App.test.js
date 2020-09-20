1f97c093a07df2a3d40f973f07378a73bddcf6a2
Nick <nearwood@gmail.com>
2020-09-20 16:54:40 -0500

    Add super basic test.

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const warningElement = getByText("Could not obtain device position.");
-  expect(warningElement).toBeInTheDocument();
+test('renders the logo', () => {
+  const { getByLabelText } = render(<App />);
+  const mobileLogo = getByLabelText("Skyanchor mobile logo");
+  expect(mobileLogo).toBeInTheDocument();
+
+  const desktopLogo = getByLabelText("Skyanchor desktop logo");
+  expect(desktopLogo).toBeInTheDocument();
 });
