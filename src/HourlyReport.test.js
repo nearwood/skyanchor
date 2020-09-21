e1a2734e285f0ee3b9740787abad9f2b47462f9c
Nick <nearwood@gmail.com>
2020-09-20 22:18:22 -0500

    Add broken snapshot tests.

--- src/HourlyReport.test.js
+++ src/HourlyReport.test.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { act } from "react-dom/test-utils";
+import pretty from "pretty";
 
 import HourlyReport from './HourlyReport';
 
 import hourlyData from './exampleData/hourly.json';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<HourlyReport hourlyData={hourlyData.properties.periods}/>);
-  //const linkElement = getByText("Temperature");
-  //expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<HourlyReport hourlyData={hourlyData.properties.periods}/>);
+//   //const linkElement = getByText("Temperature");
+//   //expect(linkElement).toBeInTheDocument();
+// });
+
+let container = null;
+let unmount = null;
+
+beforeEach(() => {
+});
+
+afterEach(() => {
+  unmount();
+  container = null;
+});
+
+xtest("Snapshot: Example HourlyReport", () => {
+  act(() => {
+    const result = render(
+      <HourlyReport hourlyData={hourlyData.properties.periods.slice(0, 8)}/>
+    );
+    container = result.container;
+    unmount = result.unmount;
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
 });
--- src/WeatherCard.test.js
+++ src/WeatherCard.test.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { act } from "react-dom/test-utils";
+import pretty from "pretty";
 
 import WeatherCard from './WeatherCard';
 
 import forecast from './exampleData/forecast.json';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<WeatherCard period={forecast.properties.periods[0]} />);
-  //const linkElement = getByText(/Today - .*/);
-  //expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<WeatherCard period={forecast.properties.periods[0]} />);
+//   //const linkElement = getByText(/Today - .*/);
+//   //expect(linkElement).toBeInTheDocument();
+// });
+
+let container = null;
+let unmount = null;
+
+beforeEach(() => {
+});
+
+afterEach(() => {
+  unmount();
+  container = null;
+});
+
+xtest("Snapshot: Example WeatherCard", () => {
+  act(() => {
+    const result = render(
+      <WeatherCard period={forecast.properties.periods[0]} />
+    );
+    container = result.container;
+    unmount = result.unmount;
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
 });
