688d6b257af7d72edf52c0ad8e317f30434bdda1
Nick <nearwood@gmail.com>
2020-09-20 21:45:47 -0500

    Add prettier for snapshot tests.

--- src/WeatherAlert.test.js
+++ src/WeatherAlert.test.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { render } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import pretty from "pretty";
 
-import WeatherAlerts from './WeatherAlerts';
+import WeatherAlerts from "./WeatherAlerts";
 
-import alerts from './exampleData/alerts.json';
+import alerts from "./exampleData/alerts.json";
 
-test('renders learn react link', () => {
-  const { container, getByText } = render(<WeatherAlerts data={alerts} open={true} onClose={jest.fn} />);
+test("renders learn react link", () => {
+  const { container, getByText } = render(
+    <WeatherAlerts data={alerts} open={true} onClose={jest.fn} />
+  );
   //const linkElement = getByText(/Today - .*/);
   //expect(linkElement).toBeInTheDocument();
 
-  expect(
-    pretty(container.innerHTML)
-  ).toMatchInlineSnapshot();
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`""`);
 });
