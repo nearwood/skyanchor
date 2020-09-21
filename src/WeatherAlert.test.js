9dc253790cff6e3dd232f456c7fbc50addcd993b
Nick <nearwood@gmail.com>
2020-09-20 22:04:27 -0500

    Add WeatherAlert Snapshots

--- src/WeatherAlert.test.js
+++ src/WeatherAlert.test.js
@@ -1,18 +1,68 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
+// import { render, unmountComponentAtNode } from "react-dom";
 import pretty from "pretty";
 
-import WeatherAlerts from "./WeatherAlerts";
+import { WeatherAlert } from "./WeatherAlerts";
 
 import alerts from "./exampleData/alerts.json";
 
-test("renders learn react link", () => {
-  const { container, getByText } = render(
-    <WeatherAlerts data={alerts} open={true} onClose={jest.fn} />
-  );
-  //const linkElement = getByText(/Today - .*/);
-  //expect(linkElement).toBeInTheDocument();
+let container = null;
+let unmount = null;
 
-  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`""`);
+beforeEach(() => {
+});
+
+afterEach(() => {
+  unmount();
+  container = null;
+});
+
+test("Snapshot: Minor WeatherAlert", () => {
+  act(() => {
+    const result = render(
+      <WeatherAlert alert={alerts.features[0]} />
+    );
+    container = result.container;
+    unmount = result.unmount;
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
+});
+
+test("Snapshot: Moderate WeatherAlert", () => {
+  act(() => {
+    const result = render(
+      <WeatherAlert alert={alerts.features[1]} />
+    );
+    container = result.container;
+    unmount = result.unmount;
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
+});
+
+test("Snapshot: Severe WeatherAlert", () => {
+  act(() => {
+    const result = render(
+      <WeatherAlert alert={alerts.features[3]} />
+    );
+    container = result.container;
+    unmount = result.unmount;
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
+});
+
+test("Snapshot: Extreme WeatherAlert", () => {
+  act(() => {
+    const result = render(
+      <WeatherAlert alert={alerts.features[4]} />
+    );
+    container = result.container;
+    unmount = result.unmount;
+  });
+
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
 });
